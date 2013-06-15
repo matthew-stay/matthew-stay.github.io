060c962440c558b904f9812a55c32ea493abbea0
Matt Stay <Matthew@staygraphic.com>
2013-06-15 16:48:34 -0400

    alternative method of detecting and hiding mobile address bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 jQuery(document).ready(function($) {
     $("h1 span, h2").lettering();
 
-	$('body').scrollTop(1);
-    
+   	window.addEventListener("load",function() {
+    setTimeout(function() {
+        document.body.scrollTop || window.scrollTo(0, 1);
+    }, 0);
+});
 });

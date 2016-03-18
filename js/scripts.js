0c9b735ce0cd1a841601ab9447990f4c19a89888
matthew-stay <hello@mattstay.com>
2016-03-17 21:24:43 -0400

    switch to isotope

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
-$(function(){
+// $(function(){
+//
+//
+// });
 
-    $('#grid-archive').find('.archive-image').lazyLoadXT();
+$.lazyLoadXT.updateEvent+=' layoutComplete';
 
+var $grid = $('#grid').isotope({
+  // options
+  itemSelector: '.item',
+  transitionDuration: 0,
+  percentPosition: true,
+masonry: {
+  // use element for option
+  columnWidth: '.grid-sizer',
+  gutter: '.gutter-sizer'
+}
+});
+
+$(window).load(function(){
+  $('#grid').find('.item').lazyLoadXT();
+});
+
+$grid.imagesLoaded().progress( function() {
+  $grid.isotope('layout');
 });

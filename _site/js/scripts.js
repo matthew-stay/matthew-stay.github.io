e4c8588d72d3be473a0d077d6bb9ecada02f31da
Matt Stay <hello@mattstay.com>
2017-04-05 21:28:30 +0700

    Updated header

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -3,6 +3,33 @@
 //
 // });
 
+$.ajax({
+        url: "http://api.tumblr.com/v2/blog/mattstay.tumblr.com/posts?api_key=v2O16ifk6m9cXWfrQkanMtfJwcmowKUmVniQKo6yxfcEJSyKuQ",
+        dataType: 'jsonp',
+        success: function(posts){
+          var postings = posts.response.posts;
+          console.log(postings);
+          var text = '';
+
+    for (var i in postings) {
+           var p = postings[i];
+          text += '<p></p>';
+      if(p.type == 'photo'){
+        text +='<div class="item"><img src=' +  p.photos[0].original_size.url + '>' + p.caption + '</div>'
+      }
+        if(p.type == 'video'){
+        text += '<div class="video-container">' + p.player[0].embed_code + '</div>' +'<p></p>' + p.caption;
+      }
+          if(p.type == 'text'){
+        text += p.body + p.caption;
+      }
+    text += '<a href='+ p.post_url +'>'+ p.post_url +'</a></li>';
+          text += '<br></br>';
+          }
+          $('#archive').append(text);
+        }
+    });
+
 $.lazyLoadXT.updateEvent+=' layoutComplete';
 
 var $grid = $('#grid').isotope({
@@ -23,4 +50,24 @@ $(window).load(function(){
 
 $grid.imagesLoaded().progress( function() {
   $grid.isotope('layout');
+});
+
+var $archive = $('#archive').isotope({
+  // options
+  itemSelector: '.item',
+  transitionDuration: 0,
+  percentPosition: true,
+masonry: {
+  // use element for option
+  columnWidth: '.grid-sizer',
+  gutter: '.gutter-sizer'
+}
+});
+
+$(window).load(function(){
+  $('#archive').find('.item').lazyLoadXT();
+});
+
+$archive.imagesLoaded().progress( function() {
+  $archive.isotope('layout');
 });
